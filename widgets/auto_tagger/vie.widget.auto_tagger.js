a3098b730427135d842cf4fde070c0e8a4f9be4b
Sebastian Germesin <neogermi@googlemail.com>
2012-02-14 12:07:55 +0100

    updated auto tagger to newest version

--- widgets/auto_tagger/vie.widget.auto_tagger.js
+++ widgets/auto_tagger/vie.widget.auto_tagger.js
@@ -9,10 +9,11 @@
         _create: function () {
             var self = this;
             for (var s in this.options.services) {
-                if (this.options.services[s].service) {
+                if (this.options.services[s].service && typeof this.options.services[s].service === "function") {
                     var options = 
                         (this.options.services[s].options)? 
                                 this.options.services[s].options : {};
+                    options.name = s;
                     this.options.vie.use(new this.options.services[s].service(options));
                 }
             }
@@ -39,15 +40,15 @@
                     continue;
                 }
                 this.options.vie.analyze({element: elem})
-                .from(s).execute()
+                .using(s).execute()
                 .done(function (entities) {
                     self.options.entities = self.options.entities.concat(entities);
                     self._unduplicateEntities();
                     var render = (self.options.render)? self.options.render : self._render;
                     render.call(self, self.options.entities);
                 })
-                .fail(function () {
-                    debugger;
+                .fail(function (e) {
+                    console.warn(e);
                 });
 
                 this._trigger('start_query', undefined, {service : s, time: new Date()});
@@ -78,17 +79,7 @@
                             var prop = la.split(".")[1];
                             if (entities[e].isof(type) &&
                                 entities[e].has(prop)) {
-                                var lit = entities[e].get(prop);
-                                if ($.isArray(lit)) {
-                                    var tmp = $.merge([], lit);
-                                    lit = tmp[0];
-                                    for (var l = 0; l < tmp.length; l++) {
-                                        if (tmp[l].indexOf('@en') > 0) {
-                                            lit = tmp[l];
-                                        }
-                                    }
-                                }
-                                label = lit.replace(/"/g, "").replace(/@[a-z]+/, '');
+                                label = self._extractString(entities[e], [ prop ], self.options.lang);
                                 break;
                             }
                         } else if (typeof la === "function") {
@@ -117,9 +108,9 @@
         
         _unduplicateEntities: function () {
             for (var i = 0; i < this.options.entities.length; i++) {
-                var iid = this.options.entities[i].id;
+                var iid = this.options.entities[i].getSubject();
                 for (var j = i+1; j < this.options.entities.length; j++) {
-                    var jid = this.options.entities[j].id;
+                    var jid = this.options.entities[j].getSubject();
                     if (iid === jid) {
                         this.options.entities.splice(j, 1);
                         j--;
@@ -128,9 +119,50 @@
             }
             return this;
         },
+        
+        _extractString : function(entity, attrs, lang) {
+            lang = (lang)? lang : this.options.lang;
+            lang  = (jQuery.isArray(lang))? lang : [ lang ];
+            for (var l = 0; l < lang.length; l++) {
+                if (entity && typeof entity !== "string") {
+                    var possibleAttrs = (_.isArray(attrs))? attrs : [ attrs ];
+                    for (var p = 0; p < possibleAttrs.length; p++) {
+                        var attr = possibleAttrs[p];
+                        if (entity.has(attr)) {
+                            var name = entity.get(attr);
+                            if (name.isCollection) {
+                                name.each(function (model) {
+                                    var val = model.get("value");
+                                    if (val.indexOf('@' + lang[l]) > -1) {
+                                        name = val;
+                                        return true;
+                                    }
+                                });
+                                if (name.isCollection) {
+                                    name = name.at(0).get("value");
+                                }
+                            }
+                            else if (jQuery.isArray(name) && name.length > 0) {
+                                for ( var i = 0; i < name.length; i++) {
+                                    if (name[i].indexOf('@' + lang[l]) > -1) {
+                                        name = name[i];
+                                        break;
+                                    }
+                                }
+                                if (jQuery.isArray(name))
+                                    name = name[0]; // just take the first
+                            }
+                            return (name) ? name.replace(/"/g, "").replace(/@[a-z]+/, '').trim() : name;
+                        }
+                    }
+                }
+            }
+            return undefined;
+        },
                 
         options: {
             vie         : new VIE(),
+            lang        : ["en"],
             append      : true,
             label       : [],
             filter      : [],
@@ -138,9 +170,7 @@
                 'stanbol' : {
                     use: false,
                     options: {
-                        url : "http://dev.iks-project.eu:8081",
-                        proxyDisabled: true,
-                        proxyUrl: "../../../utils/proxy/proxy.php"
+                        url : "http://dev.iks-project.eu:8081"
                     }
                 },
                 'rdfa' : {
